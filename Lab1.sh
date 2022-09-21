864afba83214079f26c2a132d22be1e9f3cd4626
Knyazsibiryak <113039314+Knyazsibiryak@users.noreply.github.com>
2022-09-21 18:28:12 +0300

    Update Lab1.sh

--- Lab1.sh
+++ Lab1.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 read -p "Введите ссылку на репозиторий: " URL
-cd /home/knyazsibiryak/rty
-
+cd ./rty
 git clone $URL
 FOUND="$(ls -tr | tail)"
-mkdir /media/knyazsibiryak/Rubbish/Ubunta/work1
-mv /home/knyazsibiryak/rty/"${FOUND}"/* /media/knyazsibiryak/Rubbish/Ubunta/work1
-rm -rf /home/knyazsibiryak/rty/"${FOUND}"
-sudo chmod g+rw /media/knyazsibiryak/Rubbish/Ubunta/work1
+ mkdir ./work1
+ cp "${FOUND}"/* work1
+ rm -rf "${FOUND}"
+sudo chmod g+rw ./work1
 
-echo "Лабороторная работа №1 выполнена"
-
-BASE=$(basename ${URL})
-FILE=$(printf '%b' ${BASE//%/\\x})
+#BASE=$(basename ${URL})
+#FILE=$(printf '%b' ${BASE//%/\\x})
